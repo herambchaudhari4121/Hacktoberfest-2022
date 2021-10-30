0dd3cf9f2bd6484cfb50ccbb4dbb2c9e62b99639
Kirill <59337566+kirill010403@users.noreply.github.com>
2021-10-30 12:51:55 +0300

    Output price money
    We enter a coin and our program displays the price of the coin and looks at the change in relation to the old

--- hacktober_selenium.py
+++ hacktober_selenium.py
@@ -1,25 +1,29 @@
-from selenium import webdriver                                          #селентиум
-import time                                                             #временной отрезок
-from selenium.webdriver.common.by import By                             #импортируем by
+import requests
+import time
+from bs4 import BeautifulSoup
 
-driver = webdriver.Firefox()                                            #говорим что у нас Firefox, драйвер который будет взаимодейтвовать с ним напрямую
-driver.get('https://accounts.google.com/signin/v2/identifier?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&hl=ru&flowName=GlifWebSignIn&flowEntry=ServiceLogin')                                        #вызываем youtube
-time.sleep(1.5)                                                         #ставим задержку на 1.5 секунды, чтобы успела прогрузиться страница
-# try:                                                                  #при получении ссылки на верификацию,
-# активируем ее
-#
-# except: element = driver.find_element(By.XPATH,
-# """"/html/body/ytd-app/ytd-consent-bump-v2-lightbox/tp-yt-paper-dialog/dia[2]/div[2]/div[5]/div[
-# 2]/ytd-button-renderer[2]""") element.click()
+price = 0
 
-search_bar = driver.find_element(By.XPATH, """//input[@id="search"]""")     #находим поле для ввода текста(поисковой запрос)
-search = 'pochta'                                                            #вводим запрос в консоль
-search_bar.send_keys(search)                                                #вводим запрос
-searchbutton = driver.find_element(By.XPATH, '//*[@id="search-icon-legacy"]')       #находим кнопку
-searchbutton.click()                                                                #нажимаем кнопку
-                                                   #нажимаем на него
+val = input('Введите монету\n')
+val = val.upper()
+url = "https://www.binance.com/api/v3/ticker/price?symbol=" + val + "USDT"
 
+price_old = 0
+int = 1
 
-"""
-https://selenium-python.readthedocs.io/locating-elements.html
-"""
+while True:
+    response = requests.get(url)
+    soup = str(BeautifulSoup(response.text, "html.parser"))
+    # print(soup[29:36])
+    time.sleep(0.1)
+    price = float(soup[29:36])
+    if price > price_old:
+        print(int, '. ', price, '  ', '↑')
+    elif price == price_old:
+        print(int, '. ', price, '  ', '→')
+    else:
+        print(int, '. ', price, '  ', '↓')
+    price_old = price
+    time.sleep(9)
+    int += 1
+    # print(type(price[0]))
